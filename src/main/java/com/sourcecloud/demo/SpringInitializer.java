c7a40696829bd74cc4346f5732615627452c9b93
Prashanth Yerroju <prashanthyerroju@Prashanths-MacBook-Pro.local>
2018-09-22 13:27:32 -0400

    changes

--- src/main/java/com/sourcecloud/demo/SpringInitializer.java
+++ src/main/java/com/sourcecloud/demo/SpringInitializer.java
@@ -1,13 +1,9 @@
 package com.sourcecloud.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan("com.sourcecloud")
 public class SpringInitializer {
 
 	public static void main(String[] args) {
